e546d34f8cb05089023f283d6317ee5fc27f8790
laryssadrr <laryssadantas256@gmail.com>
2025-05-26 15:15:38 -0300

    Update StatusPedido.java
    Retirou finalizado

--- src/main/java/br/edu/uepb/sistemarestaurante/models/StatusPedido.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/StatusPedido.java
@@ -1,14 +1,13 @@
 package br.edu.uepb.sistemarestaurante.models;
 
 public enum StatusPedido {
-    PENDENTE, PREPARANDO, PRONTO, ENTREGUE, FINALIZADO;
+    PENDENTE, PREPARANDO, PRONTO, ENTREGUE;
 
     public static StatusPedido novoStatus (StatusPedido atual) {
         switch (atual) {
             case PENDENTE:  return PREPARANDO;
             case PREPARANDO:     return PRONTO;
             case PRONTO:   return ENTREGUE;
-            case ENTREGUE:   return FINALIZADO;
             default:        throw new IllegalArgumentException("Status desconhecido");
         }
     }
