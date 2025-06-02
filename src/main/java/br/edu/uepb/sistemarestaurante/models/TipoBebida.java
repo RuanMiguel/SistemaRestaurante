795810d4a7d2e96019cc0c12354eb21d6d72e7fb
laryssadrr <laryssadantas256@gmail.com>
2025-06-02 11:33:39 -0300

    Update enums TipoBebida, TipoPrato e VolumeBebida

--- src/main/java/br/edu/uepb/sistemarestaurante/models/TipoBebida.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/TipoBebida.java
@@ -1,8 +1,27 @@
 package br.edu.uepb.sistemarestaurante.models;
 
 public enum TipoBebida {
-    AGUA,
-    SUCO,
-    REFRIGERANTE,
-    ALCOOLICA
+    AGUA("Àguas"),
+    SUCO("Sucos"),
+    REFRIGERANTE("Refrigerantes"),
+    ALCOOLICA("Alcoólicas");
+
+    private final String tipo;
+
+    TipoBebida(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static TipoBebida fromTipo(String tipo) {
+        for (TipoBebida TipoB : TipoBebida.values()) {
+            if (TipoB.getTipo().equals(tipo)) {
+                return TipoB;
+            }
+        }
+        throw new IllegalArgumentException("Nenhum tipo de bebida chamado " + tipo);
+    }
 }
--- src/main/java/br/edu/uepb/sistemarestaurante/models/TipoPrato.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/TipoPrato.java
@@ -1,8 +1,27 @@
 package br.edu.uepb.sistemarestaurante.models;
 
 public enum TipoPrato {
-    ENTRADA,
-    PRINCIPAL,
-    ACOMPANHAMENTO,
-    PRATO_FEITO
+    ENTRADA("Entradas"),
+    PRINCIPAL("Principais"),
+    ACOMPANHAMENTO("Acompanhamentos"),
+    PRATO_FEITO("Pratos Feitos");
+
+    private final String tipo;
+
+    TipoPrato(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static TipoPrato fromTipo(String tipo) {
+        for (TipoPrato tipoP : TipoPrato.values()) {
+            if (tipoP.getTipo().equals(tipo)) {
+                return tipoP;
+            }
+        }
+        throw new IllegalArgumentException("Nenhum tipo de prato chamado " + tipo);
+    }
 }
