7698c9c3453bd25bd77179febfa529420383e21d
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-06-07 14:01:18 -0300

    feat: adiciona fxml e controller de Cozinha

--- src/main/java/br/edu/uepb/sistemarestaurante/controllers/CozinhaController.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/controllers/CozinhaController.java
@@ -1,6 +1,8 @@
 package br.edu.uepb.sistemarestaurante.controllers;
 
+import br.edu.uepb.sistemarestaurante.models.StatusPedido;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
@@ -9,5 +11,25 @@ public class CozinhaController {
     private Button botaoVoltar;
 
     @FXML
-    private VBox pedidosVBox;
+    private VBox containerPedido; //VBox pedido modelo vazio
+
+    public void exibirPedidos(List<Pedido> pedidos) {
+        containerPedido.getChildren().clear();
+
+        for (Pedido pedido : pedidos) {
+            if (pedido.getStatus() == StatusPedido.PENDENTE || pedido.getStatus() == StatusPedido.PREPARANDO) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/edu/uepb/sistemarestaurante/views/PedidoItemView.fxml"));
+                    VBox pedidoBox = loader.load();
+
+                    PedidoItemController controller = loader.getController();
+                    controller.setPedido(pedido);
+
+                    containerPedido.getChildren().add(pedidoBox);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
