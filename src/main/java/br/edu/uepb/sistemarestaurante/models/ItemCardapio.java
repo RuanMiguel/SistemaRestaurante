80f2822ca89cd44163ca9dfb3b31c8119dbbc88c
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-06-15 20:18:39 -0300

    refactor: adiciona javaDoc para itemCardapio

--- src/main/java/br/edu/uepb/sistemarestaurante/models/ItemCardapio.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/ItemCardapio.java
@@ -3,28 +3,63 @@ package br.edu.uepb.sistemarestaurante.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe abstrata que representa um item genérico do cardápio de um restaurante.
+ * Serve como base para outros tipos específicos de itens do cardápio (Prato, Sobremesa, Bebida).
+ * Mantém uma lista estática compartilhada de todos os itens do cardápio criados.
+ *
+ * @author Letícia B. M. da Cruz
+ * @see Prato
+ * @see Sobremesa
+ * @see Bebida
+ */
 public abstract class ItemCardapio {
     private String nome;
     private double preco;
 
+    /**
+     * Lista estática compartilhada que armazena todos os itens do cardápio criados.
+     * Acessível pelas subclasses (Prato, Sobremesa, Bebida).
+     */
     protected static List<ItemCardapio> itensCardapio = new ArrayList<>();
 
-    public ItemCardapio(String nome, double preco){
+    /**
+     * Construtor para criar um novo item do cardápio.
+     *
+     * @param nome O nome do item do cardápio
+     * @param preco O preço do item
+     */
+    public ItemCardapio(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    public String getNome(){
+    /**
+     * Retorna o nome do item do cardápio.
+     *
+     * @return O nome do item
+     */
+    public String getNome() {
         return nome;
     }
 
-    public double getPreco(){
+    /**
+     * Retorna o preço do item do cardápio.
+     *
+     * @return O preço do item
+     */
+    public double getPreco() {
         return preco;
     }
 
+    /**
+     * Retorna uma representação em string do item do cardápio,
+     * incluindo seu nome e preço formatado.
+     *
+     * @return String formatada no padrão: "Nome | Preço: R$ X.XX"
+     */
     @Override
-    public String toString(){
+    public String toString() {
         return nome + " | Preço: R$ " + preco;
     }
-
 }
