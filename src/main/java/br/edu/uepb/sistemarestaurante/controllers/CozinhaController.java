5db15a3495c53cbf126a87914da4f8f8fdcc3e0b
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-06-16 02:39:03 -0300

    save alteracoes

--- src/main/java/br/edu/uepb/sistemarestaurante/controllers/CozinhaController.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/controllers/CozinhaController.java
@@ -1,5 +1,6 @@
 package br.edu.uepb.sistemarestaurante.controllers;
 
+import br.edu.uepb.sistemarestaurante.models.GerenciadorCozinha;
 import br.edu.uepb.sistemarestaurante.models.Pedido;
 import br.edu.uepb.sistemarestaurante.utils.alertaUtils;
 import br.edu.uepb.sistemarestaurante.utils.janelaUtils;
@@ -26,6 +27,7 @@ public class CozinhaController {
     private VBox containerPedidos;
 
     private ObservableList<Pedido> pedidosObservable = FXCollections.observableArrayList();
+    private GerenciadorCozinha gc = new GerenciadorCozinha();
 
     @FXML
     private void initialize() {
@@ -53,10 +55,13 @@ public class CozinhaController {
             }
 
             for (Pedido pedido : pedidosObservable) {
-                FXMLLoader loader = new FXMLLoader(fxmlUrl);
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(CAMINHO_PEDIDO_COZINHA_VIEW));
                 VBox pedidoBox = loader.load();
                 PedidoCozinhaController controller = loader.getController();
+
                 controller.setPedido(pedido);
+                controller.setGerenciadorCozinheiro(new GerenciadorCozinha()); // ou um GC já existente
+
                 containerPedidos.getChildren().add(pedidoBox);
             }
         } catch (IOException e) {
