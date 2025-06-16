052327171f987124506e7a75660c97161b72dfcf
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-06-15 22:13:55 -0300

    feat: adiciona Javadoc para Sobremesa

--- src/main/java/br/edu/uepb/sistemarestaurante/models/Sobremesa.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/Sobremesa.java
@@ -1,10 +1,34 @@
 package br.edu.uepb.sistemarestaurante.models;
 
-public class Sobremesa extends ItemCardapio{
+/**
+ * Classe que representa um item de sobremesa no cardápio do restaurante.
+ * Herda de ItemCardapio e representa itens sobremesas do restaurante.
+ *
+ * @author [Seu Nome ou Equipe]
+ * @version 1.0
+ */
+public class Sobremesa extends ItemCardapio {
 
-    public Sobremesa(String nome, double preco){
+    /**
+     * Construtor para criar uma instância de Sobremesa.
+     *
+     * @param nome  Nome da sobremesa (não pode ser nulo ou vazio)
+     * @param preco Preço da sobremesa (deve ser positivo)
+     * @throws IllegalArgumentException Se nome for nulo/vazio ou preço for negativo
+     */
+    public Sobremesa(String nome, double preco) {
         super(nome, preco);
         itensCardapio.add(this);
+    }
+
+    /**
+     * Retorna uma representação em string da sobremesa.
+     *
+     * @return String formatada com informações da sobremesa
+     */
+    @Override
+    public String toString() {
+        return String.format("Sobremesa: %s [Preço: R$%.2f]", getNome(), getPreco());
     }
 }
 
