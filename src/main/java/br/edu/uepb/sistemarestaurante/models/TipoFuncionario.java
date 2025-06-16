27b9adec82606b656b17a7d753f19fa4a5a01413
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-06-15 22:22:54 -0300

    feat: adiciona Javadoc para TipoFuncionario

--- src/main/java/br/edu/uepb/sistemarestaurante/models/TipoFuncionario.java
+++ src/main/java/br/edu/uepb/sistemarestaurante/models/TipoFuncionario.java
@@ -1,6 +1,21 @@
 package br.edu.uepb.sistemarestaurante.models;
 
+/**
+ * Enumeração que representa os tipos de funcionários do sistema de restaurante.
+ * Utilizado para controle de acesso e definição de permissões no sistema.
+ *
+ * @author Letícia B. M Cruz
+ */
 public enum TipoFuncionario {
+    /**
+     * Representa um garçom, responsável pelo gerenciamento de pedidos nas mesas.
+     */
     GARCOM,
-    COZINHA
+
+    /**
+     * Representa o gerenciador da cozinha, responsável pel
+     * atualização do status dos pedidos.
+     */
+    COZINHA;
+
 }
